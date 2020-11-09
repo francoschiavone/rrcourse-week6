5e27cf2eb08162cbdc00f98a34393e6693887d68
francoschiavone <francoaschiavone@gmail.com>
2020-11-09 13:05:59 -0300

    added walls

--- js/game.js
+++ js/game.js
@@ -2,8 +2,10 @@ var canvas = null,
     ctx = null,
     lastPress = null,
     pause = true,
+    gameover = true,
     score = 0,
     dir = 0,
+    wall = new Array(),
     player = null,
     food = null;
 
@@ -47,7 +49,20 @@ function random(max) {
   return Math.floor(Math.random() * max);
 }
 
+function reset() {
+  score = 0;
+  dir = 1;
+  player.x = 40;
+  player.y = 40;
+  food.x = random(canvas.width / 10 - 1) * 10;
+  food.y = random(canvas.height / 10 - 1) * 10;
+  gameover = false;
+}
+
 function paint(ctx) {
+  var i = 0,
+      l = 0;
+
   // Clean canvas
   ctx.fillStyle = '#09232f';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -55,6 +70,12 @@ function paint(ctx) {
   // Draw player
   ctx.fillStyle = '#fff';
   player.fill(ctx);
+
+  // Draw walls
+  ctx.fillStyle = '#999';
+  for (i = 0, l = wall.length; i < l; i += 1) {
+    wall[i].fill(ctx);
+  }
 
   // Draw food
   ctx.fillStyle = '#337ab7';
@@ -76,7 +97,15 @@ function paint(ctx) {
 }
 
 function act() {
+  var i,
+      l;
+
   if (!pause) {
+    // GameOver Reset
+    if (gameover) {
+      reset();
+    }
+
     // Change Direction
     if (lastPress == KEY_UP) {
       dir = 0;
@@ -125,6 +154,19 @@ function act() {
       food.x = random(canvas.width / 10 - 1) * 10;
       food.y = random(canvas.height / 10 - 1) * 10;
     }
+
+    // Wall Intersects
+    for (i = 0, l = wall.length; i < l; i += 1) {
+      if (food.intersects(wall[i])) {
+        food.x = random(canvas.width / 10 - 1) * 10;
+        food.y = random(canvas.height / 10 - 1) * 10;
+      }
+
+      if (player.intersects(wall[i])) {
+        gameover = true;
+        pause = true;
+      }
+    }
   }
 
   // Pause/Unpause
@@ -152,6 +194,12 @@ function init() {
   // Create player and food
   player = new Rectangle(40, 40, 10, 10);
   food = new Rectangle(80, 80, 10, 10);
+
+  // Create walls
+  wall.push(new Rectangle(100, 50, 10, 10));
+  wall.push(new Rectangle(100, 100, 10, 10));
+  wall.push(new Rectangle(200, 50, 10, 10));
+  wall.push(new Rectangle(200, 100, 10, 10));
 
   // Start game
   run();
